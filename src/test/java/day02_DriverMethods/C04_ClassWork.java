be733c51f9885e019e2136a9c8205e9a25ffe425
AlperenYcdl <yucedalalper@gmail.com>
2022-12-23 19:47:20 +0100

    S

--- src/test/java/day02_DriverMethods/C04_ClassWork.java
+++ src/test/java/day02_DriverMethods/C04_ClassWork.java
@@ -1,19 +1,19 @@
 package day02_DriverMethods;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.awt.*;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class C04_ClassWork {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();//Acilan browser'i tam ekran yap
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));//gecisleri bekletmek icin
+
         //web sayfasına gidin. https://www.amazon.com/
         driver.get("https://www.amazon.com/");
 
@@ -21,9 +21,21 @@ public class C04_ClassWork {
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("city bike", Keys.ENTER);
 
         //Amazon'da görüntülenen ilgili sonuçların sayısını yazdırın
-        WebElement aramaSonucYazisi = driver.findElement(By.className("sg-col-inner"));
-        System.out.println(aramaSonucYazisi.getText());
+        List<WebElement> aramaSonucYazisi = driver.findElements(By.className("sg-col-inner"));
+        System.out.println("Arama sonuc yazisi : " + aramaSonucYazisi.get(0).getText());
+
+        //arama sonuc sayisini console a yazdiriniz
+        String[] sonucSayisi = aramaSonucYazisi.get(0).getText().split(" ");
+        System.out.println("Sonuc sayisi : " + sonucSayisi[0]); //yukarida boldu ve 0 ci indexi verdi
+
+        //Sonuc sayisini LAMBDA ile yazdiriniz
+        Arrays.stream(aramaSonucYazisi.get(0).getText().split(" ")).limit(3).skip(2).forEach(System.out::println);
+
 
         //Sonra karşınıza çıkan ilk sonucun resmine tıklayın.
+        List<WebElement> ilkWE=driver.findElements(By.className("s-image"));
+        ilkWE.get(0).click();
+
     }
+
 }
